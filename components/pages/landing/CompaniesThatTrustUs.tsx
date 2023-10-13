d9091c53f814752d3ab8df0baf364ebac771cfee
Justkanye <adekanyetimilehin2000@gmail.com>
2023-10-13 22:23:03 +0100

    carousel animation

--- components/pages/landing/CompaniesThatTrustUs.tsx
+++ components/pages/landing/CompaniesThatTrustUs.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+
 import { companiesThatTrustUs } from "@/lib/constants/companiesThatTrustUs";
 import { altFromImgUrl } from "@/lib/utils";
-import Image from "next/image";
+import MotionContainer from "@/components/layout/framer-motion/MotionContainer";
+import { carousel } from "@/lib/helpers/motion";
 
 const CompaniesThatTrustUs = () => {
   return (
@@ -8,14 +11,20 @@ const CompaniesThatTrustUs = () => {
       <p className='text-gray-600 text-xl font-medium text-center'>
         Join 1,500+ companies already video conferencing the ClearLink way
       </p>
-      <div className='flex items-center gap-[69px] mt-12 overflow-x-scroll'>
-        {companiesThatTrustUs.map(({ height, src, width }) => (
-          <Image
-            key={altFromImgUrl(src)}
-            alt={altFromImgUrl(src)}
-            {...{ src, width: width, height }}
-          />
-        ))}
+      <div className='mt-12 relative'>
+        <MotionContainer
+          className='flex items-center gap-[69px] mt-12 transition-transform'
+          variants={carousel({})}
+          // animate='animate'
+        >
+          {companiesThatTrustUs.map(({ height, src, width }) => (
+            <Image
+              key={altFromImgUrl(src)}
+              alt={altFromImgUrl(src)}
+              {...{ src, width: width, height }}
+            />
+          ))}
+        </MotionContainer>
       </div>
     </section>
   );
